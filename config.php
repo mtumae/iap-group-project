ef81fed684e92fae717c04736bcbee85f6644927
happy-wondessen <happywondessen@gmail.com>
2025-09-19 16:06:10 +0300

    config.php added

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// Site Information
+$conf['site_name'] = 'MediMeet';
+$conf['site_url'] = 'http://localhost';
+$conf['admin_email'] = 'admin@strathmart.com';
+
+// Database Configuration
+$conf['db_type'] = 'pdo';
+$conf['db_host'] = 'localhost';
+$conf['db_user'] = 'root';
+$conf['db_pass'] = '1234';
+$conf['db_name'] = 'strathmart';
+
+// Site Language
+$conf['site_lang'] = 'en';
+
+
+// Email Configurations
+
+$conf['mail_type'] = 'smtp';
+$conf['smtp_host'] = 'smtp.gmail.com';
+$conf['smtp_user'] = 'happywondessen@gmail.com';
+$conf['smtp_pass'] = '';
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = 'ssl';
