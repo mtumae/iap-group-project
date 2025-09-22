b1d97be1dcbf2f525245ab158eb4d139be5916fa
happy-wondessen <happywondessen@gmail.com>
2025-09-22 08:07:48 +0300

    Staging changes before pulling

--- Forms/forms.php
+++ Forms/forms.php
@@ -1,6 +1,4 @@
 <?php 
-require_once 'plugins/PHPMailer/mail.php';
-
 
 class Forms{
 
@@ -25,6 +23,7 @@ class Forms{
         <button name='signup-form' type="submit" class="btn btn-primary">Submit</button>
         </form>
         <?php
+
         if(isset($_POST['signup-form'])){
         $errors = [];
 
@@ -49,12 +48,11 @@ class Forms{
         }
 
         if (empty($errors)) {
-            $mail = new Mail();
             $code = rand(100000, 999999);
             echo "<p style='color: green;'>Form submitted successfully!</p>";
             echo $code;
-            $mail->verifyAccount($_POST['email'], $code);
             $this->twofactorauth($code);
+
         } else {
             // Display errors to the user and re-display the form
             foreach ($errors as $error) {
@@ -69,18 +67,38 @@ class Forms{
     public function twofactorauth($code){
         ?>
         <form action="" method="POST">
-            <div class="form-group">
-                <label for="verification-code">Verification Code</label>
-                <input type="text" class="form-control" id="verification-code" name='verification-code' aria-describedby="codeHelp" placeholder="Enter verification code">
-                <small id="codeHelp" class="form-text text-muted">Please enter the 6-digit code sent to your email.</small>
-            </div>
+        <div class="form-group">
+            <label for="verification-code">Verification Code</label>
+            <input type="text" class="form-control" id="verification-code" name='verification-code' aria-describedby="codeHelp" placeholder="Enter verification code">
+            <small id="codeHelp" class="form-text text-muted">Please enter the 6-digit code sent to your email.</small>
+        </div>
+       
         <button name='2fa-form' type="submit" class="btn btn-primary">Submit</button>
         </form>
         <?php
 
-<<<<<<< HEAD
+        if(isset($_POST['2fa-form'])){
+        $errors = [];
+
+        //check if field is filled
+        if (empty($_POST['verification-code'])) {
+            $errors[] = "Please enter the verification code!";
+        }
+
+        //check if code is exactly 6 digits long and only contains numbers
+        if (!preg_match('/^\d{6}$/', $_POST['verification-code'])) {
+            $errors[] = "Invalid verification code! Must be exactly 6 digits long.";
+        }
+
+        if (empty($errors) and $_POST['verification-code'] === $code) {
+            echo "<p style='color: green;'>Verification successful!</p>";
+        } else {
+            // Display errors to the user and re-display the form
+            foreach ($errors as $error) {
+                echo "<p style='color: red;'>$error</p>";
+            }
+        }
+
     }
 }
-=======
-    }}
->>>>>>> cf4d4b0ea678a3720e149a7729906b50a41aa3bf
+}
