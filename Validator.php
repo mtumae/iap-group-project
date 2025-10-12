8c0d0ec21ad3d2c9b62c92b8d841cd40dd3f557d
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-10-12 11:02:30 +0300

    deleted Validator

--- Validator.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Validator {
-    // Password must be at least 8 chars, 1 upper, 1 lower, 1 number, 1 special
-    public static function isStrongPassword($password) {
-        return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/', $password);
-    }
-}
